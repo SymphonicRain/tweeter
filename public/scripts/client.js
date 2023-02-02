cdfa372e5171624375f4d09a43d4fe6f21929324
RJ <randy.j416@yahoo.com>
2023-02-01 21:25:13 -0500

    back to top button implemented

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,77 +5,66 @@
  */
 
 
-/*
-<section class="timeline">
-  <article class="tweets">
-          <header class="tweet-header"><div>username, <img src="https://s.yimg.com/cv/apiv2/default/mlb/20190319/500x500/yankees_wbgs.png" width = 50px, height="50px"></div><div>handle</div></header>
-          <p class="tweet-body">text body2 alfalfa</p>
-
-          
-          <footer class="tweet-footer">time, <div class="footer-icons"><i class="fa-solid fa-flag hovericon1"></i> <i class="fa-solid fa-retweet hovericon1"></i><i class="fa-regular fa-heart hovericon1"></i></div></footer>
-
-        </article>
-*/
-// const data = [
-//   {
-//     "user": {
-//       "name": "Newton",
-//       "avatars": "https://i.imgur.com/73hZDYK.png"
-//       ,
-//       "handle": "@SirIsaac"
-//     },
-//     "content": {
-//       "text": "If I have seen further it is by standing on the shoulders of giants"
-//     },
-//     "created_at": 1461116232227
-//   },
-//   {
-//     "user": {
-//       "name": "Descartes",
-//       "avatars": "https://i.imgur.com/nlhLi3I.png",
-//       "handle": "@rd" },
-//     "content": {
-//       "text": "Je pense , donc je suis"
-//     },
-//     "created_at": 1461113959088
-//   }
-// ]
-
-
 
 $(document).ready(function() {
   // --- our code goes here ---
   console.log("IMREADY");
   $(".errorLong").slideUp(0);
   $(".errorShort").slideUp(0);
+  $("section.new-tweet").slideUp(0);
+  $(".scroll").hide()
+
+  $(".expand").on("click", () =>{
+    const $section = $("section.new-tweet")
+    if ($section.is(":visible")) {
+      $section.slideUp();
+    } else {
+      $section.slideDown(200);
+      $section.find("textarea").focus();
+    }
+  })
+
+  $(window).scroll(function(){
+    if($(document).scrollTop() > 200){
+      $(".scroll").show();
+    }
+    if($(document).scrollTop() <= 200){
+      $(".scroll").hide();
+    }
+
+  });
+
+  $(".scroll").on("click", () =>{
+    const $section = $("section.new-tweet")
+    // if ($section.is(":visible")) {
+    //   $section.slideUp();
+    // } else {
+      $section.slideDown(200);
+      $section.find("textarea").focus();
+    // }
+  })
 
 
-  const button = $(".submit");
   $( "form" ).submit(function( event ) {
     event.preventDefault();
-    if ($( "#tweet-text" ).val().length < 1) {
+    const tweetLength = $( "#tweet-text" ).val().length;
+    if (tweetLength < 1) {
       $(".errorShort").slideDown();
       return;
     }
-    if ($( "#tweet-text" ).val().length > 140) {
+    if (tweetLength > 140) {
       $(".errorLong").slideDown();
       return;
     }
-    // $( "#tweet-text" ).serialize();
-    if (140 >= $( "#tweet-text" ).val().length > 0) {
+
+    if (140 >= tweetLength &&  tweetLength > 0) {
       $(".errorLong").slideUp(0);
       $(".errorShort").slideUp(0);
       $.post("/tweets/", $( "#tweet-text" ).serialize());
       setTimeout(()=> loadTweets(), 200);
-      $( "#tweet-text" ).val() = "";  // need to empty it after
-
-      
+      $( "#tweet-text" ).val(""); 
     }
   });
-//   const $tweet = createTweetElement(tweetData);
-
-// // Test / driver code (temporary)
-//   $('.timeline').append($tweet); 
 
 });
 
